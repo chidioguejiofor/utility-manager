2423150aeab5f053c315bec92f4c55f71449f24e
chidioguejiofor <chidioguejiofor@gmail.com>
2019-10-25 07:33:36 +0100

    ft(seeder): Infrastructure is now complete
    - adds Seeder Manager infrastructure
    - creates the UnitSeeder for seeding units
    - generates seed units in units.yaml
    - adds seed migrations for Unit model
    
    [Finishes https://trello.com/c/GZPvdF0j/15-setup-fixture-manager-and-seeders-infrastructure]

---
+++ migrations/versions/c564f76264f5_seed_unit_data.py
@@ -0,0 +1,24 @@
+"""Seed Unit data
+
+Revision ID: c564f76264f5
+Revises: a703a747118c
+Create Date: 2019-10-27 20:29:53.438696
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from seeders.seeders_manager import SeederManager
+
+# revision identifiers, used by Alembic.
+revision = 'c564f76264f5'
+down_revision = 'a703a747118c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    SeederManager.seed_database('unit')
+
+
+def downgrade():
+    pass

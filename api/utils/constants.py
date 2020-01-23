a8d589284d5c4321ece66910276c00c8f7766a35
chidioguejiofor <chidioguejiofor@gmail.com>
2020-01-23 19:06:36 +0100

    fix(sentry): sentry ignore errors
    - makes sentry to ignore some custom errors

--- api/utils/constants.py
+++ api/utils/constants.py
@@ -6,3 +6,8 @@ RESET_PASSWORD_SUBJECT = 'Reset Password'
 CONFIRM_TOKEN = 0
 LOGIN_TOKEN = 1
 RESET_TOKEN = 2
+SENTRY_IGNORE_ERRORS = [
+    'api.utils.exceptions.UniqueConstraintException',
+    'api.utils.exceptions.ResponseException',
+    'api.utils.exceptions.ModelOperationException',
+]
